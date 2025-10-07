5de1519a86e71572ea606d729c91c29cf5c64c5d
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2025-10-07 13:18:33 -0700

    Time: 1 ms (89.94%), Space: 56.9 MB (71.06%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -3,6 +3,33 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
+    // O(1) additional space approach: 
+    let mode = -1;
+    let count = 0;
+    for (var i = 0; i < nums.length; i++) {
+        if (count === 0) {
+            mode = nums[i];
+            count++;
+        } else {
+            if (nums[i] === mode) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+    }
+    count = 0;
+    for (var i = 0; i < nums.length; i++) {
+        if (nums[i] === mode) {
+            count++;
+        }
+    }
+    if (count > (Math.floor(nums.length / 2))) {
+        return mode;
+    } 
+    return -1;
+};
+var majorityElementNaive = function(nums) {
     // naive approach: frequency map and return key with highest number
         // O(N) time and O(N) additional space
     let counts = {};
