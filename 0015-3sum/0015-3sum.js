66de131efcc893f2eb57c4f40ac2416a8834a4f3
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2025-10-13 15:38:19 -0700

    Time: 25 ms (98.73%), Space: 73.1 MB (26.7%) - LeetHub

---
+++ 0015-3sum/0015-3sum.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+    nums.sort((a, b) => a - b);
+    let triplets = [];
+    for (var i = 0; i < nums.length; i++) {
+        if (nums[i] > 0) {
+            break;
+        }
+        if (i === 0 || (nums[i] !== nums[i - 1])) {
+            let j = i + 1;
+            let k = nums.length - 1;
+            while (j < k) {
+                let total = nums[i] + nums[j] + nums[k];
+                if (total > 0) {
+                    k--;
+                } else if (total < 0) {
+                    j++;
+                } else {
+                    triplets.push([nums[i], nums[j], nums[k]]);
+                    j++;
+                    while (nums[j] === nums[j - 1] && j < k) {
+                        j++;
+                    }
+                }
+            } 
+        }
+    }
+    return triplets;
+};
