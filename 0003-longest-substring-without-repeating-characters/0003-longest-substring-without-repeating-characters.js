6b90d5d19240f2afe3ad98aaf998e991ea7a3f3f
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2025-10-13 12:14:30 -0700

    Time: 147 ms (8.56%), Space: 63.8 MB (6.08%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -0,0 +1,52 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    if (s.length <= 1) {
+        return s.length;
+    }
+    // frequency map that gets reinitialized
+    let counts = {};
+    // sliding window:
+    let left = 0;
+    let right = 1;
+    let maxLength = 0;
+    counts[s.charAt(0)] = 1;
+    while (left < right && left >= 0 && right < s.length) {
+        if (counts.hasOwnProperty(s.charAt(right))) {
+            // close window from left until right is add-able
+            while (counts.hasOwnProperty(s.charAt(right)) && left < right) {
+                delete counts[s.charAt(left)];
+                left++;
+            }
+        }
+        counts[s.charAt(right)] = 1;
+        maxLength = Math.max(maxLength, Object.keys(counts).length);
+        right++;
+    }
+    return maxLength;
+};
+var lengthOfLongestSubstringBrute = function(s) {
+    // frequency map that gets reinitialized
+    let counts = {};
+    // brute force: iterate through all substrings
+    // sliding window: o
+    let maxLength = 0;
+    let invalid = false;
+    for (var i = 0; i< s.length; i++) {
+        counts = {};
+        invalid = false;
+        for (var j = i; j < s.length; j++) {
+            if (counts.hasOwnProperty(s.charAt(j))) {
+                invalid = true;;
+            } else {
+                if (!invalid) {
+                    counts[s.charAt(j)] = 1;
+                }
+            }
+        }
+        maxLength = Math.max(maxLength, Object.keys(counts).length);
+    }
+    return maxLength;
+};
