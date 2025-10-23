95172034ca133e8149c283d1876f146e5cd15b1e
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2025-10-23 14:52:21 -0700

    Time: 52 ms (66.02%), Space: 56.6 MB (84.15%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.js
+++ 0133-clone-graph/0133-clone-graph.js
@@ -14,22 +14,18 @@ var cloneGraph = function(node) {
     if (!node) {
         return node;
     }
-    let clone = new Node(node.val);
     // to avoid infinite looping, store nodes as "visited" somehow
     let visited = [];
-    traverse(node, clone, visited);
-    return clone;
+    return traverse(node, visited);;
 };
-var traverse = function(node, clone, visited) {
-    visited[clone.val] = clone; // mark curr as visited by setting it to be cloned version
-    // console.log(node.val, 'has neighbors', node.neighbors);
-    node.neighbors.forEach((neighbor) => {
-        if (visited[neighbor.val]) {
-            clone.neighbors.push(visited[neighbor.val]); // add existing node
-        } else {
-            let newNode = new Node(neighbor.val); // clone node
-            clone.neighbors.push(newNode); // add new cloned node
-            traverse(neighbor, newNode, visited); // visit neighbors of "new" cloned node
-        }
-    });
+var traverse = function(node, visited) {
+    // if not already cloned
+    if (!visited[node.val]) {
+        visited[node.val] = new Node(node.val); // visit and clone curr
+        // console.log(node.val, 'has neighbors', node.neighbors);
+        node.neighbors.forEach((neighbor) => {
+            visited[node.val].neighbors.push(traverse(neighbor, visited)); // visit neighbors
+        });
+    }
+    return visited[node.val];
 }
