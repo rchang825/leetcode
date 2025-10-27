67f74554d66d99a3b9a8703ed68fc56524b9cb86
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2025-10-26 19:30:43 -0700

    Time: 1 ms (61.75%), Space: 59 MB (43.24%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.js
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.js
@@ -17,41 +17,25 @@ var isValidBST = function(root) {
     // return false early if curr.right <= curr.val
     // also check if all nodes to in right subtree are greater than root
     // left and right subtrees must also be valid
-    
-    // base case: root is null, return true
-    if (root === null) {
+    let prev;
+    var validate = function(curr) {
+        // base case: root is null, return true
+        if (curr === null) {
+            return true;
+        }
+        // left
+        if (!validate(curr.left)) {
+            return false;
+        }
+        if (prev && prev.val >= curr.val) {
+            return false;
+        }
+        prev = curr;
+        // right
+        if (!validate(curr.right)) {
+            return false;
+        }
         return true;
-    }
-    // base case: left is greater
-    if (root.left && root.left.val >= root.val) {
-        console.log('left is not valid');
-        return false;
-    }
-    // base case: right is lesser
-    if (root.right && root.right.val <= root.val) {
-        console.log('right is not valid');
-        return false;
-    }
-    if (checkLeft(root.left, root) && checkRight(root.right, root)) {
-        return isValidBST(root.left) && isValidBST(root.right);
-    }
-    return false;
+    };
+    return validate(root);
 };
-var checkLeft = function(curr, root) {
-    if (curr === null) {
-        return true;
-    }
-    if (curr.val >= root.val) {
-        return false;
-    }
-    return checkLeft(curr.left, root) && checkLeft(curr.right, root);
-}
-var checkRight = function(curr, root) {
-    if (curr === null) {
-        return true;
-    }
-    if (curr.val <= root.val) {
-        return false;
-    }
-    return checkRight(curr.left, root) && checkRight(curr.right, root);
-}
