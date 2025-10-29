a1da4e74845cd93acba19496c08ac891954e6cb8
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2025-10-29 12:26:48 -0700

    Time: 0 ms (100%), Space: 55.7 MB (50.91%) - LeetHub

--- 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.js
+++ 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.js
@@ -3,6 +3,13 @@
  * @return {number}
  */
 var smallestNumber = function(n) {
+    let power = 0;
+    while (n >= 2 ** power) {
+        power++;
+    }
+    return 2 ** power - 1;
+}
+var smallestNumberBrute = function(n) {
     // convert n to binary rep as a string
     let binaryArr = n.toString(2).split('');
     // set all 0's to 1's
