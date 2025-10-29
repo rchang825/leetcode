c8825a9baeae8ab5914c3442d224769f7f19d52f
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2025-10-29 11:46:44 -0700

    Time: 0 ms (100%), Space: 54.3 MB (63.72%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -4,28 +4,33 @@
  * @return {number}
  */
 var search = function(nums, target) {
+    // [0, 1, 2, 4, 5, 6, 7]
+    // [4, 5, 6, 7, 0, 1, 2] target = 0
     let left = 0;
     let right = nums.length - 1;
-    while (left < right) {
+    while (left <= right) {
         let mid = Math.floor((left + right) / 2);
-        if (nums[mid] > nums[right]) { // not sorted
-            if (nums[mid] < target || target <= nums[right]) { // target in left half
-                left = mid + 1; 
-            } else { // target not in right half
-                right = mid;
-            }
-        } else {
-            if (nums[mid] < target && target <= nums[right]) { // target in right half, sorted
+        if (target === nums[mid]) {
+            return mid;
+        }
+
+        if (nums[left] <= nums[mid]) {
+            if (target < nums[mid] && target >= nums[left]) {
+                right = mid - 1;
+            } else {
                 left = mid + 1;
-            } else { // target not in right half
-                right = mid;
+            }
+        }
+        if (nums[mid] <= nums[right]) {
+            if (target > nums[mid] && target <= nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
     }
-    if (left === right) {
-        if (target === nums[left]) {
-            return left;
-        }
-        return -1;
-    }
+    // if (nums[left] === target) {
+    //     return left;
+    // }
+    return -1;
 };
