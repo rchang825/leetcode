16cae001690d5ff26ea9dd5cedff56c43779e2d0
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2025-11-03 12:50:15 -0800

    Time: 2 ms (19.15%), Space: 61.8 MB (5.29%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.js
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.js
@@ -10,7 +10,39 @@
  * @param {number} val
  * @return {ListNode}
  */
-var removeElements = function(head, val) {
+var removeElements = function(head, val, curr) {
+    // console.log('curr', curr);
+    if (curr === undefined) {
+        // while head exists and should be deleted
+        while (head && head.val === val) {
+            // head = head.next
+            head = head.next;
+        }
+        curr = head;  
+        // console.log('curr = head', curr);
+    }
+    // base case: curr is null
+    if (curr === null) {
+        // return the "new" head
+        return head;
+    } else {
+        // set a nextCurr pointer
+        // console.log('curr', curr);
+        let nextCurr = curr.next;
+        // while next node exists and should be deleted
+        while (nextCurr && nextCurr.val === val) {
+            // move nextCurr pointer
+            nextCurr = nextCurr.next;
+        }
+        // set curr.next to be nextCurr
+        curr.next = nextCurr;
+        // return removeElements(head, val, curr.next)
+        // console.log('going to recurse now! curr = ', curr, 'curr.next=', curr.next);
+        return removeElements(head, val, curr.next);
+    }
+};
+
+var removeElementsIterative = function(head, val) {
     while (head && head.val === val) {
         head = head.next;
     }
