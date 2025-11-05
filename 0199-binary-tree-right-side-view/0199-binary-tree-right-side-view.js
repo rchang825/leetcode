46a18ad07ec77a8fb44cf3d14f6728ef5b265558
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2025-11-05 12:02:22 -0800

    Time: 1 ms (15.25%), Space: 56 MB (25.52%) - LeetHub

---
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.js
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var rightSideView = function(root) {
+    // level order traversal but add right children first
+    // keep track of height and only add first of height to res
+    // root = height 0 -> add 1
+    // 3 (h=1), 2 (h=1) -> add 3
+    // 4 (h=2) -> add 4
+    // 5 (h=3) -> add 5
+
+    let queue = [];
+    let res = [];
+    queue.push([root, 0]);
+    // console.log(queue);
+    while (queue.length !== 0) {
+        let curr = queue.shift();
+        let height = curr[1];
+        // console.log(curr[0]);
+        if (curr[0] !== null) {
+            res.push(curr[0].val);
+            // console.log('adding', curr[0].val);
+            queue.push([curr[0].right, height + 1]);
+            queue.push([curr[0].left, height + 1]);
+            // console.log(queue);
+            // console.log('going to check for same height', height, 'nodes to ignore...');
+            while (queue.length !== 0 && height === queue[0][1]) {
+                let ignored = queue.shift();
+                if (ignored[0] !== null) {
+                    queue.push([ignored[0].right, ignored[1] + 1]);
+                    queue.push([ignored[0].left, ignored[1] + 1]);
+                }
+            } // ignore all other nodes on same level but add their children!
+        }
+    }
+    return res;
+};
