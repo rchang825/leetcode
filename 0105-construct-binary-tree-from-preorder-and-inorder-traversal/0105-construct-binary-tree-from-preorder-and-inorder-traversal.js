f01ce61879ca9531b19e5ec5b0e3040503e5d353
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2025-11-06 17:41:18 -0800

    Time: 80 ms (13.89%), Space: 132.6 MB (14.49%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.js
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.js
@@ -12,7 +12,6 @@
  * @return {TreeNode}
  */
 var buildTree = function(preorder, inorder) {
-    // console.log('pre', preorder, 'in', inorder);
     if (preorder.length === 0 || inorder.length === 0) {
         return null;
     }
@@ -21,12 +20,7 @@ var buildTree = function(preorder, inorder) {
     while (inorder[i] !== root.val) {
         i++;
     }
-    // everything before i is on curr's left branch
-    // everything after i is on curr's right branch
-    // console.log('finding curr.left of', curr.val);
     root.left = buildTree(preorder.slice(1), inorder.slice(0, i));
-    // console.log('finding curr.right of', curr.val);
     root.right = buildTree(preorder.slice(i + 1), inorder.slice(i + 1));
-    // console.log(root.val);
     return root;
 };
