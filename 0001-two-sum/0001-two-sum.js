9d4ae563a5b1703d22b895f9ea86d0fa5453fe78
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2025-11-11 18:37:45 -0800

    Time: 6 ms (47.89%), Space: 55.8 MB (62.62%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -13,28 +13,33 @@ var twoSum = function(nums, target) {
     // make a frequency map of nums that tracks indices
     let counts = {};
     for (var n = 0; n < nums.length; n++) {
+        let complement = target - nums[n];
+        if (counts.hasOwnProperty(complement)) {
+            return [n, counts[complement][0]];
+        }
         if (counts.hasOwnProperty(nums[n])) {
             counts[nums[n]].push(n);
         } else {
             counts[nums[n]] = [n];
         }
     }
+    return [];
     // console.log(counts);
     // iterate through nums
-    for (var i = 0; i < nums.length; i++) {
-        // fix the current number and look for a complement
-        let complement = target - nums[i];
-        if (counts.hasOwnProperty(complement)) {
-            // check for edge case
-            if (complement === nums[i]) {
-                if (counts[complement].length >= 2) {
-                   return [counts[complement][0], counts[complement][1]]; 
-                }
-            } else {
-                return [i, counts[complement][0]];
-            }
-        }
-            // return indices if found
-    }
-    return [];
+    // for (var i = 0; i < nums.length; i++) {
+    //     // fix the current number and look for a complement
+    //     let complement = target - nums[i];
+    //     if (counts.hasOwnProperty(complement)) {
+    //         // check for edge case where addends are the same
+    //         if (complement === nums[i]) {
+    //             if (counts[complement].length >= 2) {
+    //                return [counts[complement][0], counts[complement][1]]; 
+    //             }
+    //         } else {
+    //             return [i, counts[complement][0]];
+    //         }
+    //     }
+    // }
+    // // given there is guaranteed to be a valid answer, this should never execute
+    // return [];
 };
