812695cec5fb3f3490153c27ff25ae8bab2af3e4
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2025-11-11 19:26:05 -0800

    Time: 1 ms (87.81%), Space: 57.2 MB (36.04%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,38 +4,13 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    /*
-    I: array of integers, target integer
-    O: array of two indices [i, j] where nums[i] + nums[j] = target
-    C: you may not use same element twice
-    E: element appears twice -> return 2 different indices
-    */
-    // make a frequency map of nums that tracks indices
     let counts = {};
-    for (var n = 0; n < nums.length; n++) {
-        let complement = target - nums[n];
+    for (var i = 0; i < nums.length; i++) {
+        let complement = target - nums[i];
         if (counts.hasOwnProperty(complement)) {
-            return [n, counts[complement]];
+            return [i, counts[complement]];
         }
-        counts[nums[n]] = n;
+        counts[nums[i]] = i;
     }
     return [];
-    // console.log(counts);
-    // iterate through nums
-    // for (var i = 0; i < nums.length; i++) {
-    //     // fix the current number and look for a complement
-    //     let complement = target - nums[i];
-    //     if (counts.hasOwnProperty(complement)) {
-    //         // check for edge case where addends are the same
-    //         if (complement === nums[i]) {
-    //             if (counts[complement].length >= 2) {
-    //                return [counts[complement][0], counts[complement][1]]; 
-    //             }
-    //         } else {
-    //             return [i, counts[complement][0]];
-    //         }
-    //     }
-    // }
-    // // given there is guaranteed to be a valid answer, this should never execute
-    // return [];
 };
