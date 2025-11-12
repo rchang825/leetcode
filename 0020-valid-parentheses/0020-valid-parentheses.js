22e27aae9a3ce5c7b55c0c34bc5fef5d677ecc2a
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2025-11-12 12:18:25 -0800

    Time: 1 ms (90%), Space: 57.6 MB (35.84%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -3,31 +3,37 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    let opening = {'(' : ')', '{': '}', '[': ']'};
-    // stack of characters
-    let stack = [];
-    // iterate through all characters in s
-    let i = 0;
-    while (i < s.length) {
-        // while not on closing char, add to stack
-        if (opening.hasOwnProperty(s.charAt(i))) {
-            stack.push(s.charAt(i));
-        }
-        
-        else {
-            // while still on closing chars, check if it is the counterpart to stack.pop
-            if (opening[stack.pop()] !== s.charAt(i)) {
-                // if not, return false
-                // console.log('does not match!!')
-                return false;
-            }
-        }
-        i++;
-    }
-    // return true if stack is empty
-    if (stack.length === 0) {
-        return true;
-    } 
-    // console.log('open chars remaining!')
+  // special case: length of s is odd -> can't be valid
+  if (s.length % 2 === 1) {
     return false;
+  }
+  // define complements
+  let closing = {
+    '(': ')',
+    '[': ']',
+    '{': '}'
+  };
+  let stack = [];
+  // iterate through s
+  for (var i = 0; i < s.length; i++) {
+    // while current character is an opening parenthesis
+    if (closing.hasOwnProperty(s[i])) {
+        // push onto stack
+        stack.push(s[i]);
+    } else {
+        // at this point the first element off the stack must correspond to the next closing parenthesis
+        // if that is true, pop the first element off
+        let curr = stack[stack.length - 1];
+        if (curr && closing[curr] === s[i]) {
+            stack.pop();
+        } else {
+            return false;
+        }
+    }
+  }
+  // if the stack is empty
+    // return true
+  // otherwise, that means there are unclosed parentheses
+    // return false
+    return stack.length === 0;
 };
