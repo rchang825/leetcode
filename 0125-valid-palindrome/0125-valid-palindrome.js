58e3e7f1036f6fa18211aa11f8beace7b2c43f56
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2025-11-18 12:38:31 -0800

    Time: 2 ms (93.12%), Space: 58.5 MB (44.62%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -3,6 +3,27 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
+    var isValidChar = function(ch) {
+        return (ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9') || (ch >= 'A' && ch <= 'Z');
+    }
+    let left = 0;
+    let right = s.length - 1;
+    while (left < right) {
+        while (left < right && !isValidChar(s[left])) {
+            left++;
+        };
+        while (left < right && !isValidChar(s[right])) {
+            right--;
+        }
+        if ((s[left] + '').toLowerCase() !== (s[right] + '').toLowerCase()) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
+}
+var isPalindromeReverse = function(s) {
     s = s.toLowerCase();
     let strArr = [];
     // iterate through s to remove non-alphanumeric chars and convert to lowercase
