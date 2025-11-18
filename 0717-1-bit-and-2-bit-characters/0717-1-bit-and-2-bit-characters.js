4b095ed6b0824ebf731d184c864fecdc369cbaaa
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2025-11-18 14:23:34 -0800

    Time: 101 ms (0.89%), Space: 63.5 MB (5.36%) - LeetHub

---
+++ 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} bits
+ * @return {boolean}
+ */
+var isOneBitCharacter = function(bits) {
+    let two = 0;
+    for (var i = 0; i < bits.length; i++) {
+        console.log(bits[i]);
+        if(two === 1) {
+            console.log('10 or 11 at', i);
+            two = 2;
+        } else if (bits[i]) {
+            console.log('starts with 1 at', i);
+            two = 1;
+        } else {
+            two = 0;
+        }
+    }
+    return two === 0;
+};
