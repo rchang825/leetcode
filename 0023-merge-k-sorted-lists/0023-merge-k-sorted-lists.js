511f8b614d1a5af5778820f1484b571fbd9679da
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2025-11-19 11:30:40 -0800

    Time: 1 ms (99.74%), Space: 60.9 MB (95.56%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.js
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.js
@@ -10,20 +10,53 @@
  * @return {ListNode}
  */
 var mergeKLists = function(lists) {
+    var mergeLists = function(list1, list2) {
+        if (list1 === null) {
+            return list2;
+        }
+        if (list2 === null) {
+            return list1;
+        }
+        let p1 = list1;
+        let p2 = list2;
+        if (list2.val < list1.val) {
+            p1 = list2;
+            p2 = list1;
+        }
+        let root = new ListNode();
+        let curr = root;
+        while (p1 !== null && p2 !== null) {
+            if (p1.val < p2.val) {
+                curr.next = p1;
+                p1 = p1.next;
+                curr = curr.next;
+            } else {
+                curr.next = p2;
+                p2 = p2.next;
+                curr = curr.next;
+            }
+        }
+        if (p1 === null) {
+            curr.next = p2;
+        } else {
+            curr.next = p1;
+        }
+        
+        return root.next;
+    };
+    if (lists.length === 0) {
+        return null;
+    }
     let root = new ListNode();
     let curr = root;
-    let all = [];
-    for (var list of lists) {
-        let listPointer = list;
-        while (listPointer !== null) {
-            all.push(listPointer.val);
-            listPointer = listPointer.next;
+    let numLists = lists.length;
+    let interval = 1;
+    while (interval < numLists) {
+        // console.log(lists);
+        for (var i = 0; i < numLists - interval; i += interval * 2) {
+            lists[i] = mergeLists(lists[i], lists[i + interval]);
         }
+        interval *= 2;
     }
-    all.sort((a,b) => a - b);
-    all.forEach((n) => {
-        curr.next = new ListNode(n);
-        curr = curr.next;
-    });
-    return root.next;
+    return lists[0];
 };
