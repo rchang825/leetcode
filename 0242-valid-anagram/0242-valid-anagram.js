ef753c0da974e91e263f8e2d4fd623460505a65d
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2025-11-20 18:52:09 -0800

    Time: 31 ms (15.76%), Space: 58 MB (58.6%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,25 +4,7 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    // frequency map of s and match it to t
-    let map = {};
-    for (var i = 0; i < s.length; i++) {
-        if (map.hasOwnProperty(s[i])) {
-            map[s[i]]++;
-        } else {
-            map[s[i]] = 1;
-        }
-    }
-    for (var j = 0; j < t.length; j++) {
-        if (map.hasOwnProperty(t[j])) {
-            if (map[t[j]] === 1) {
-                delete map[t[j]];
-            } else {
-                map[t[j]]--;
-            }
-        } else {
-            return false;
-        }
-    }
-    return Object.keys(map).length === 0;
+    // turn strings into character arrays, sort, then back to strings
+    // compare strings
+    return s.split('').sort().join('') === t.split('').sort().join('');
 };
