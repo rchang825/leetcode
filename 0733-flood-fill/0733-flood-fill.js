b6ac94d4852467bafc528a968ef0267b47867249
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2025-11-22 12:32:08 -0800

    Time: 0 ms (100%), Space: 59.3 MB (19.39%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.js
+++ 0733-flood-fill/0733-flood-fill.js
@@ -6,32 +6,25 @@
  * @return {number[][]}
  */
 var floodFill = function(image, sr, sc, color) {
-    // save image[sr][sc]'s current color as prev
-    let prev = image[sr][sc];
-    if (prev === color) {
-        return image;
+    var helper = function(r, c, currColor) {
+        // check for in bounds
+        if (r < 0 || r >= image.length || c < 0 || c >= image[0].length) {
+            return;
+        }
+        // if same color, change and recurse on adjacent
+        if (image[r][c] === currColor) {
+            image[r][c] = color;
+            helper(r - 1, c, currColor);
+            helper(r + 1, c, currColor);
+            helper(r, c - 1, currColor);
+            helper(r, c + 1, currColor);
+        } else {
+            return;
+        }
     }
-    floodFillHelper(image, sr, sc, color, prev);
+    if (image[sr][sc] !== color) {
+        helper(sr, sc, image[sr][sc]);
+    }
+    
     return image;
-};
-var floodFillHelper = function(image, sr, sc, color, ogColor) {
-    // return if either index is out of bounds
-    // console.log('sr:', sr, 'sc:', sc);
-    if ((sr > (image.length - 1)) || (sr < 0) || (sc > (image[sr].length - 1)) || (sc < 0)) {
-        // console.log('out of bounds, return');
-        return;
-    }
-    if (image[sr][sc] !== ogColor) {
-    // return if curr is not ogColor
-        // console.log('not ogColor, return');
-        return;
-    }
-    image[sr][sc] = color;
-    // console.log(`painted [${sr},${sc}]: ${image}`);
-    // recurse with up, down, left, right, of curr
-    floodFillHelper(image, sr - 1, sc, color, ogColor);
-    floodFillHelper(image, sr + 1, sc, color, ogColor);
-    floodFillHelper(image, sr, sc - 1, color, ogColor);
-    floodFillHelper(image, sr, sc + 1, color, ogColor);
-    // return image;
 };
