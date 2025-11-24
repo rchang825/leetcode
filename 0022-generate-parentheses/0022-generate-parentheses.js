64d03ec5f63f97744543e1f835c489152ac5fd7d
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2025-11-24 17:01:07 -0500

    Time: 1 ms (47%), Space: 55.6 MB (54.83%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.js
+++ 0022-generate-parentheses/0022-generate-parentheses.js
@@ -3,67 +3,6 @@
  * @return {string[]}
  */
 var generateParenthesis = function(n) {
-    // I: number (of pairs)
-    // O: array of strings, each string contains well-formed parentheses
-    // C: number of pairs is at least 1 and no more than 8
-    // E: 
-    
-
-    // n = 1
-    // "()"
-    // n = 2
-    // "()" -> "()()", adding on another set
-    // "()" -> (())", nesting
-    // n = 3
-    // "()()" -> "()()()", adding on another set
-    // "(())" -> "(())()", adding on another set
-    // "(())" -> "()(())", adding on another set
-    // "(())" -> "((()))", 
-    //           "(()())"
-
-    /*
-        given ()
-        insert ()
-        - end ()()
-        - beginning -> duplicate ()()
-        - inside (())
-        given ()()
-        insert ()
-        - end ()()()
-        - beginning -> duplicate ()()()
-        - inside (())()
-        - inside ()(())
-        - outside (()())
-
-        ( -> (( or ()
-        (( -> ((( or (()
-        (() -> (()( or (())
-        () -> ()(
-        put in all possible places for open parentheses
-        fill in with closing parentheses
-        n = 3
-        (----)
-
-        (((--- -> ((())) 
-        ((-(-- 
-        ((--(-
-        (-((--
-        (-(-(-
-
-        ())(()
-        (--((-
-
-        n = 4
-        ((((----
-
-        1st open paren up to i = 0 (1st spot) 2 * 1 - 1
-        ((--|----
-        (-(-|---- 2nd open paren up to i = 2 (3rd spot) 2 * 2 - 1
-        ((--(--- (())()() 3rd open paren up to i = 4 (5th spot) 2 * 4 - 1
-        2 * (numOpen + 1)
-        n = 1
-        (-
-    */
     // define a res array
     let res = [];
     // define length as n * 2
@@ -71,10 +10,8 @@ var generateParenthesis = function(n) {
     // keep track of i (where you're fixing open paren)
     // helper (curr, i, numOpen)
     var helper = function(curr, i, numOpen) {
-        // console.log('curr starts off', curr.join(''));
         // if numOpen is 0, 
         // fix the character as ( at i
-        // console.log('adding new open paren at', i);
         curr[i] = '('
         // if number of open paren == n
         if (numOpen + 1 === n) {
@@ -84,14 +21,12 @@ var generateParenthesis = function(n) {
                     curr[j] = ')';
                 }
             }
-            // console.log('adding to res', curr.join(''));
             // convert curr to string, add to res array
             res.push(curr.join(''));
         } else {
             // otherwise, from i + 1 to length - 1, 
             // call helper(curr, new index)
             for (var k = i + 1; k <= 2 * (numOpen + 1); k++) {
-                // console.log('recursing to add another open paren');
                 helper(curr.slice(), k, numOpen + 1);
             }
         }
