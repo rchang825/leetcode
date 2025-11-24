972a9e2c78238380afe7921b31e123ed9a8c68fa
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2025-11-24 17:32:45 -0500

    Time: 70 ms (29.62%), Space: 65.8 MB (5.39%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.js
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.js
@@ -13,21 +13,19 @@
  * @return {TreeNode}
  */
 var lowestCommonAncestor = function(root, p, q) {
-    // returns if p or q doesn't exist within subtree
-    // or tree starts off null
-    if (!root) {
-        return null;
-    }
-    // edge case: a node can be a descendant of itself
-    if (root === p || root === q) {
+    // edge case: tree starts empty or a node can be a descendant of itself
+    if (!root || root === p || root === q) {
         return root;
     }
     // recurse based on binary search criteria
+    // p and q must both be in left subtree
     if (root.val > q.val && root.val > p.val) {
         return lowestCommonAncestor(root.left, p, q);
     } 
+    // p and q must both be in right subtree
     if (root.val < p.val && root.val < q.val) {
         return lowestCommonAncestor(root.right, p, q);
     }
+    // p and q are in different subtrees (lc ancestor found!)
     return root;
 };
