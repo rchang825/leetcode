e3c7be8e8ccb6b6498efa2a4bc12e6249473ccc0
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2025-11-26 15:05:46 -0800

    Time: 1729 ms (12.5%), Space: 122.4 MB (25%) - LeetHub

--- 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.js
+++ 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.js
@@ -3,69 +3,37 @@
  * @param {number} k
  * @return {number}
  */
-var numberOfPathsTLE = function(grid, k) {
-    // initialize numPaths = 0
-    let numPaths = 0;
-    let m = grid.length;
-    let n = grid[0].length;
-    // find a path with a helper(currSum, r, c)
-    var helper = function(currSum, r, c) {
-        // base case: r or c is out of bounds
-        if (r >= m || c >= n) {
-            return;
-        }
-        // update currSum
-        currSum += grid[r][c];
-        currSum %= k;
-        // base case: r,c is at bottom right
-        if (r === m - 1 && c === n - 1) {
-            // add 1 to numPaths if sum % k = 0
-            if (currSum === 0) {
-                numPaths++;
-                numPaths %= (10**9 + 7);
-            }
-        } else {
-        // otherwise:
-            // recurse on currSum, down
-            helper(currSum, r + 1, c);
-            // recurse on currSum, right
-            helper(currSum, r, c + 1);
-        }
-    }
-    // call helper(0, 0, 0)
-    helper(0, 0, 0);
-    // return numPaths
-    return numPaths;
-};
 var numberOfPaths = function(grid, k) {
-    let m = grid.length;
-    let n = grid[0].length;
-    // set up a memo array
-    let memo = [];
-    // console.log(memo);
-    for (var i = 0; i <= m; i++) {
-        memo[i] = [];
-        for (var j = 0; j <= n; j++) {
-            memo[i][j] = new Array(k).fill(0);
-            // initialize top left value which only has one possible remainder
-            if (i === 1 && j === 1) {
-                memo[i][j][grid[0][0] % k] = 1;
-            } else {
-                if (i >= 1 && j >= 1) {
-                    let remainder = grid[i - 1][j - 1] % k;
-                    for (var r = 0; r < k; r++) {
-                        const prevRemainder = (r - remainder + k) % k;
-                        // up
-                        memo[i][j][r] += memo[i - 1][j][prevRemainder];
-                        //left
-                        memo[i][j][r] += memo[i][j - 1][prevRemainder];
-                        // prevent overflow
-                        memo[i][j][r] %= 10 ** 9 + 7;
-                    }
-                }
-            }
+    // define constants
+    let M = grid.length;
+    let N = grid[0].length;
+    let MOD = 10 ** 9 + 7;
+    // define memo array
+    let memo = new Array(M + 1).fill().map(el => new Array(N + 1).fill().map(elem => new Array(k).fill(0)));
+
+    // helper(row, col, remainder)
+    var helper = function(r, c, rem) {
+        // base case: row or col out of bounds
+        if (r < 0 || r >= M || c < 0 || c >= N) {
+            // return 0
+            return 0;
         }
-    }
-    // return memo[bottom][right][0]
-    return memo[m][n][0];
+        // base case: at bottom right
+        if (r === M - 1 && c === N - 1) {
+            // return if the remainder is 0 (valid path)
+            return (rem + grid[r][c]) % k === 0 ? 1 : 0;
+        }
+        // return memoized result if applicable
+        if (memo[r][c][rem] !== 0) {
+            return memo[r][c][rem];
+        }
+        // recurse on down
+        let down = helper(r + 1, c, (rem + grid[r][c]) % k);
+        // recurse on right
+        let right = helper(r, c + 1, (rem + grid[r][c]) % k);
+        memo[r][c][rem] = (down + right) % MOD;
+        return memo[r][c][rem];
+    };
+    // call helper with top left element and remainder of 0
+    return helper(0, 0, 0);
 };
