b470e5b027d7fa2dd7ff203d476458c3716f4998
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2025-11-29 15:54:44 -0800

    Time: 1 ms (77.78%), Space: 57.2 MB (47.22%) - LeetHub

--- 3512-minimum-operations-to-make-array-sum-divisible-by-k/3512-minimum-operations-to-make-array-sum-divisible-by-k.js
+++ 3512-minimum-operations-to-make-array-sum-divisible-by-k/3512-minimum-operations-to-make-array-sum-divisible-by-k.js
@@ -4,11 +4,17 @@
  * @return {number}
  */
 var minOperations = function(nums, k) {
+    return nums.reduce((sum, num) => sum + num, 0) % k;
+};
+
+var minOperationsInitial = function(nums, k) {
     // initialize sum as 0
     let sum = 0;
     // iterate thrugh current nums to find sum
     nums.forEach((n) => {
         sum += n;
     });
+    // each operation will decrement sum by 1
+    // so the min number of operations = sum % k
     return sum % k;
 };
