0ef0d32a5e50910bbd880a054a079cdae1d2e3fb
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2025-12-02 15:59:15 -0800

    Time: 47 ms (73.41%), Space: 55.9 MB (25.03%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.js
+++ 0190-reverse-bits/0190-reverse-bits.js
@@ -8,11 +8,8 @@ var reverseBits = function(n) {
     // pad to 32 bits
     let pad = 32 - bin.length;
     bin = new Array(pad).fill('0').join('') + bin;
-    // console.log('og', bin);
     // reverse
     let rev = bin.split('').reverse().join('');
-    // console.log('rev', rev);
     // convert back to decimal
-    // console.log(parseInt(rev, 2));
     return parseInt(rev, 2);
 };
