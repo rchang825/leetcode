e1a0b457dd597cbd963ec4b8b7ac7432b7d969d5
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2025-12-02 16:22:24 -0800

    Time: 6 ms (96.64%), Space: 56.5 MB (96.48%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.js
+++ 0383-ransom-note/0383-ransom-note.js
@@ -5,6 +5,42 @@
  */
 var canConstruct = function(ransomNote, magazine) {
     // build frequency map of letters in ransomNote
+    // can use an array of size 26 because all letters are lowercase
+    let counts = new Array(26);
+    let aChar = 'a'.charCodeAt(0);
+    for (var i = 0; i < ransomNote.length; i++) {
+        let code = ransomNote.charCodeAt(i) - aChar;
+        if (counts[code]) {
+            counts[code]++;
+        } else {
+            counts[code] = 1;
+        }
+    }
+    // define a length variable that = ransomNote.length
+    let length = ransomNote.length;
+    // iterate through magazine
+    let l = 0;
+    while (l < magazine.length) {
+        // if ransomNote uses a letter in magazine
+        let currCode = magazine.charCodeAt(l) - aChar;
+        if (counts[currCode]) {
+            // decrement it
+            counts[currCode]--;
+            // decrement length
+            length--;
+        }
+        // if length is ever 0
+        if (length === 0) {
+            // return true early
+            return true;
+        }
+        l++;
+    }
+    // return false otherwise
+    return false;
+}
+var canConstruct1 = function(ransomNote, magazine) {
+    // build frequency map of letters in ransomNote
     let counts = {};
     for (var i = 0; i < ransomNote.length; i++) {
         if (counts.hasOwnProperty(ransomNote[i])) {
