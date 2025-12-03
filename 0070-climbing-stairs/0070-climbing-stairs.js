37963f5abb09383cec1a7ebe69b16e4fd7c0dee6
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2025-12-03 12:58:08 -0800

    Time: 0 ms (100%), Space: 53.7 MB (75.72%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.js
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -2,21 +2,14 @@
  * @param {number} n
  * @return {number}
  */
-    
-var climbStairs = function(n) {
-    let memo = {};
-    memo[0] = 0;
-    // base cases: 1 and 2
-    memo[1] = 1;
-    memo[2] = 2;
-    return memoizeStairs(n, memo);
-};
-var memoizeStairs = function(count, memo) {
-    let n = parseInt(count);
-    if (memo.hasOwnProperty(n)) {
-        return memo[n];
+var helper = function(count, memo) {
+    if (memo[count]) {
+        return memo[count];
     }
-    // all other cases will be climbStairs(n - 1) + climbStairs(n - 2);
-    memo[n] = (memoizeStairs(n-1, memo) + memoizeStairs(n - 2, memo));
-    return memo[n];
+    memo[count] = helper(count - 1, memo) + helper(count - 2, memo);
+    return memo[count];
 }
+var climbStairs = function(n) {
+    let memo = [0, 1, 2];
+    return helper(n, memo);
+};
