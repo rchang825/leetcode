704b754bcf707719438363389b06c91044394fac
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2025-12-04 14:41:35 -0800

    Time: 657 ms (5.05%), Space: 70.2 MB (5%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -3,50 +3,27 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    // O(1) additional space approach: 
-    let mode = -1;
-    let count = 0;
-    for (var i = 0; i < nums.length; i++) {
-        if (count === 0) {
-            mode = nums[i];
-            count++;
+    // define target of more than n / 2
+    let target = Math.floor(nums.length / 2) + 1;
+    let count = {};
+    let i = 0;
+    // frequency map
+    while (i < nums.length) {
+        if (count.hasOwnProperty(nums[i])) {
+            count[nums[i]]++;
         } else {
-            if (nums[i] === mode) {
-                count++;
-            } else {
-                count--;
-            }
+            count[nums[i]] = 1;
         }
-    }
-    count = 0;
-    for (var i = 0; i < nums.length; i++) {
-        if (nums[i] === mode) {
-            count++;
+        console.log(count);
+        // if anything reaches target
+        if (count[nums[i]] === target) {
+            // immediately return it
+            return nums[i];
         }
+        i++;
     }
-    if (count > (Math.floor(nums.length / 2))) {
-        return mode;
-    } 
+    // assuming majority element always exists
+    // code will never reach here
+    // but you can return -1 for debugging
     return -1;
-};
-var majorityElementNaive = function(nums) {
-    // naive approach: frequency map and return key with highest number
-        // O(N) time and O(N) additional space
-    let counts = {};
-    nums.forEach((n) => {
-        if (counts.hasOwnProperty(n)) {
-            counts[n] = counts[n] + 1;
-        } else {
-            counts[n] = 1;
-        }
-    });
-    let modeCount = 0;
-    let mode = null;
-    for (var uniqueNum in counts) {
-        if (counts[uniqueNum] > modeCount) {
-            modeCount = counts[uniqueNum];
-            mode = parseInt(uniqueNum);
-        }
-    }
-    return mode;
 };
