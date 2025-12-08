47e868fc4a21ad3a1eaf3d4d2f28b114b6950a0b
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2025-12-07 21:28:01 -0800

    Time: 28 ms (30.77%), Space: 54.2 MB (71.15%) - LeetHub

---
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countTriples = function(n) {
+    // define res
+    let res = 0;
+    // from 1 to n
+    for (var i = 1; i <= n; i++) {
+        // fix first num a -> get square
+        let triple = i * i;
+        // from 1 to  n - 1
+        for (var j = 1; j <= n; j++) {
+            // fix b -> get square
+            let sq = j * j;
+            triple += sq;
+            // if c exists and is whole number
+            let c = Math.sqrt(triple);
+            // console.log('looking for', i, '+', j, '=', c);
+            if (c % 1 === 0 && c <= n) {
+                // console.log(i, '+', j, '=', c);
+                // increment res
+                res++;
+            } 
+            // reset b
+            triple -= sq;
+        }
+    }          
+    // return res
+    return res;
+};
