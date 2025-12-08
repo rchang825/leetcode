c2ae7e09dd8572cf52bf18feee5f367528ddac36
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2025-12-08 14:07:20 -0800

    Time: 1 ms (21.98%), Space: 58.2 MB (5.94%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.js
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.js
@@ -11,44 +11,26 @@
  * @return {number[][]}
  */
 var zigzagLevelOrder = function(root) {
-    // I: root of a binary tree
-    // O: array of arrays, where each subarray represents a row of node values
-    // C: number of nodes in tree could be up to 2000 (pretty large)
-        // always start from left to right (even on root node)
-    // E: number of nodes in tree could be 0 (root = null)
-
-    // handle edge case: root is null, tree has 0 nodes
     if (!root) {
         return [];
     }
-    // define a res array
+
     let res = [];
-    // modified level order traversal: 
-    // define a queue
     let queue = [];
-    // add root and starting direction to queue
     queue.push(root);
-    // initialize current level array as empty array
     let currLevel = [];
-    // define a starting direction
     let startLeft = true;
-    // while the queue is not empty
     while (queue.length !== 0) {
-        // level size = current length of queue
         let size = queue.length;
-        // inner loop for size of level
         for (var i = 0; i < size; i++) {
-            // dequeue (array.shift())
             let curr = queue.shift();
-            // if (curr) {
-                currLevel.push(curr.val);
-                if (curr.left) {
-                    queue.push(curr.left);
-                }
-                if (curr.right) {
-                    queue.push(curr.right);
-                }   
-            // }
+            currLevel.push(curr.val);
+            if (curr.left) {
+                queue.push(curr.left);
+            }
+            if (curr.right) {
+                queue.push(curr.right);
+            }   
         }
         if (!startLeft) {
             currLevel = currLevel.reverse();
