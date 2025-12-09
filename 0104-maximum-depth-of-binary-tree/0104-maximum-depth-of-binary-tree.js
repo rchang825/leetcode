883f85c65776549aea0f3b347225eb53a021c6b3
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2025-12-09 12:28:22 -0800

    Time: 0 ms (100%), Space: 56.4 MB (94.93%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
@@ -11,13 +11,13 @@
  * @return {number}
  */
 var maxDepth = function(root) {
-    // recursion: 
-    // base case: root is null
-    if (root === null) {
-        // return 0
+    // base case: root is null (end of subtree or no tree at all) 
+    if (!root) {
         return 0;
     }
-    // recursive case:
-        // return 1 + max of recurse(left) and recurse(right)
+    // recursive case: 
+    // current node has a depth of 1
+    // recurse on the left tree and right tree to get depths of subtrees
+    // add the maximum of left and right tree depth
     return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
 };
