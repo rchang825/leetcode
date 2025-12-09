6c8eca71d6165419e70333e9e825716c83c02c64
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2025-12-08 23:17:26 -0800

    Time: 711 ms (9.09%), Space: 92.3 MB (63.64%) - LeetHub

---
+++ 3583-count-special-triplets/3583-count-special-triplets.js
@@ -0,0 +1,83 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var specialTriplets = function(nums) {
+    // define constants
+    let MOD = 10 ** 9 + 7;
+    // frequency map
+    let freq = {};
+    nums.forEach((num) => {
+        if (freq.hasOwnProperty(num)) {
+            freq[num]++;
+        } else {
+            freq[num] = 1;
+        }
+    });
+    // define res
+    let res = 0;
+    // define prefix array
+    let pref = {};
+    // iterate through j's
+    let left = 0;
+    let right = 0;
+    nums.forEach((num) => {
+        let target = num * 2;
+        if (pref.hasOwnProperty(target)) {
+            left = pref[target];
+        } else {
+            pref[target] = 0;
+            left = 0;
+        }
+        if (pref.hasOwnProperty(num)) {
+            pref[num]++;
+        } else {
+            pref[num] = 1;
+        }
+        right = (freq.hasOwnProperty(target) ? freq[target] : 0) - (pref.hasOwnProperty(target) ? pref[target] : 0);
+        res += (left * right) % MOD;
+        res %= MOD;
+    });
+    // return res
+    return res;
+}
+var specialTripletsWIP = function(nums) {
+    // define constants
+    let n = nums.length;
+    let MOD = 10 ** 9 + 7
+    // define bookends i and k
+    let i = 0;
+    let k = n - 1
+    // define res
+    let res = 0;
+    while(i < n - 2) {
+        if (k <= i) {
+            i++;
+            k = n - 1;
+            break;
+        }
+        if (nums[i] === nums[k]) {
+            console.log('pair found at', i, 'and', k, ':', nums[i], nums[k]);
+            // look for j
+            let target = nums[i] / 2;
+            console.log('looking for', target);
+            // return early if not a whole number
+            if (target % 1 > 0) {
+                break;
+            }
+            for(var curr = i + 1; curr < k; curr++) {
+                if (nums[curr] === target) {
+                    // target found
+                    res++;
+                    res %= MOD;
+                }
+            }
+            i++;
+            k = n - 1;
+        } else {
+            k--;
+        }
+        
+    }
+    return res;
+};
