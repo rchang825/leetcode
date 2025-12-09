e8b28227d974de4bd9c6501c3b56e9f82b2eb06e
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2025-12-09 13:43:30 -0800

    Time: 1 ms (84.34%), Space: 64.2 MB (63.24%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.js
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -3,27 +3,31 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
-    if (nums.length === 0) {
-        return 0;
-    }
-    // maxSum and currSum
-    let maxSum = nums[0];
+    // sliding window
+    // define sum
+    let sum = 0;
+    // edge case: only negative subarrays in nums
+    // initialize maxSum with max negative value
+    let maxSum = Number.NEGATIVE_INFINITY;
     let left = 0;
-    let right = 1;
-    let currSum = nums[0];
-    // iterate through nums
-    while (right < nums.length && left <= right) {
-        if (currSum < 0) {
-            // restart (close window)
-            currSum = 0;
-            left = right;
-        }
-        currSum += nums[right];
-        // always open window to right 
+    let right = 0;
+    // start from first element and sum
+    while (left <= right && right < nums.length) {
+        // update sum
+        sum += nums[right];
+        // update maxSum
+        maxSum = Math.max(maxSum, sum);
+        // if sum is negative
+        if (sum < 0) {
+            // start over
+            // sum = 0
+            sum = 0;
+            // close left
+            left++;
+        } 
+        // open window to right
         right++;
-        //update maxSum when necessary
-        maxSum = Math.max(currSum, maxSum);
     }
-    // return maxSum at end
+    // return maxSum
     return maxSum;
 };
