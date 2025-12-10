bc140172fbbf4e2b1794ba9d80403e6b7a9632a7
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2025-12-10 11:40:40 -0800

    Time: 0 ms (100%), Space: 55 MB (36.88%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -18,16 +18,21 @@ var search = function(nums, target) {
             // left is sorted
         let leftSorted = nums[left] <= nums[mid];
         // one half is guaranteed to be sorted (even if array is rotated)
+        // if left half is not sorted, that means the right half is
         if (leftSorted) {
+            // if target lies within nums[left] and nums[mid], proceed with normal binary search on left half
             if (nums[left] <= target && target < nums[mid]) {
                 right = mid - 1;
             } else {
+                // otherwise, throw left half away
                 left = mid + 1;
             }
         } else {
-            if (target > nums[mid] && target <= nums[right]) {
+            // if target lies within nums[mid] and nums[right], proceed with normal binary search on right half
+            if (nums[mid] < target && target <= nums[right]) {
                 left = mid + 1;
             } else {
+                // otherwise, throw right half away
                 right = mid - 1;
             }
         }
