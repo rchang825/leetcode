68adbccfd8c033700aadd6fc4771cb50267a5c90
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2025-12-11 20:18:24 -0800

    Time: 21 ms (75%), Space: 65.9 MB (50%) - LeetHub

--- 3433-count-mentions-per-user/3433-count-mentions-per-user.ts
+++ 3433-count-mentions-per-user/3433-count-mentions-per-user.ts
@@ -23,7 +23,7 @@ function countMentions(numberOfUsers: number, events: string[][]): number[] {
     }
     // iterate through events
     for (const e of events) {
-        console.log(e);
+        // console.log(e);
         // if MESSAGE
         if (e[0] === 'MESSAGE') {
             // if ALL
@@ -31,33 +31,33 @@ function countMentions(numberOfUsers: number, events: string[][]): number[] {
                 // increment mentions of all users
                 // iterate through mentions array
                 for(let i: number = 0; i < numberOfUsers; i++) {
-                    console.log('mentioned', i);
+                    // console.log('mentioned', i);
                     // increment by 1
                     mentions[i]++;
                 }
             } else if (e[2] === 'HERE') {
                 // checks if any offline users should come back online for timestamp
                 if (stack.length !== 1) {
-                   console.log('next user to go back online is', stack[0]); 
+                //    console.log('next user to go back online is', stack[0]); 
                 }
                 while (stack.length !== 0 && (stack[0][1] <= Number(e[1]))) {
                     // console.log('because', (stack[0][1] <= Number(e[1])), ',');
                     let user: number = stack.shift()[0];
                     // final check to make sure time off didn't get extended
                     if (offline[user] <= Number(e[1])) {
-                       console.log('user', user, 'is back online by', e[1]);
+                    //    console.log('user', user, 'is back online by', e[1]);
                        online.add(user); 
                     } 
                 }
                 // increment mentions of all online users at current timestamp
                 for(let user of online) {
-                    console.log('mentioning', user);
+                    // console.log('mentioning', user);
                     mentions[Number(user)]++;
                 }
             } else {
                 e[2].split(' ').forEach((id) => {
                     // format: `id${id}`
-                    console.log('mentioning', id);
+                    // console.log('mentioning', id);
                     mentions[Number(id.slice(2))]++;
                 });
             }
@@ -65,13 +65,13 @@ function countMentions(numberOfUsers: number, events: string[][]): number[] {
         // if OFFLINE
             // user is offline at timestamp and back online at timestamp + 60
             stack.push([Number(e[2]), Number(e[1]) + 60]);
-            console.log('online users before', online);
+            // console.log('online users before', online);
             online.delete(Number(e[2]));
-            console.log('online users after', online);
-            console.log('making', e[2], 'go OFFLINE until', Number(e[1]) + 60);
+            // console.log('online users after', online);
+            // console.log('making', e[2], 'go OFFLINE until', Number(e[1]) + 60);
             offline[e[2]] = Number(e[1]) + 60;
         }
-        console.log('**********');
+        // console.log('**********');
     }
     // return mentions
     return mentions;
