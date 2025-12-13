de51572a7b14a4c58b7024689bd03b78d4dff8dd
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2025-12-12 16:09:55 -0800

    Time: 1 ms (94.16%), Space: 58.7 MB (88.32%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.ts
+++ 0057-insert-interval/0057-insert-interval.ts
@@ -10,24 +10,23 @@ function insert(intervals: number[][], newInterval: number[]): number[][] {
     // if no overlap with newInterval
     while (i < intervals.length && intervals[i][1] < newInterval[0]) {
         // add interval to res
-        res.push(intervals[i].slice());
+        res.push(intervals[i]);
         i++;
     }
     // while newInterval overlaps
-    // curr start <= newInt end and curr end >= newInt start
+    // curr start <= newInt end
     while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
+        // newInt start = min of curr start and newInt start
         // newInt end = max of curr end and newInt end
-        console.log('comparing', newInterval, 'with', intervals[i]);
         newInterval[0] = Math.min(intervals[i][0], newInterval[0]);
         newInterval[1] = Math.max(intervals[i][1], newInterval[1]);
-        console.log(newInterval);
         i++;
     }
     // add potentially modified new interval to res
-    res.push(newInterval.slice());
+    res.push(newInterval);
     // add rest of intervals
     while (i < intervals.length) {
-        res.push(intervals[i].slice());
+        res.push(intervals[i]);
         i++;
     }
     // return res
