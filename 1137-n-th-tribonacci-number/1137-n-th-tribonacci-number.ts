970ad7c622c22beff073d36ad95a7608aa2fe0d1
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2025-12-16 11:41:50 -0800

    Time: 0 ms (100%), Space: 53.8 MB (92.31%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.ts
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.ts
@@ -1,4 +1,24 @@
 function tribonacci(n: number): number {
+    if (n < 1) {
+        return 0;
+    }
+    if (n < 3) {
+        return 1;
+    }
+    let a = 0;
+    let b = 1;
+    let c = 1;
+    
+    let res = 0;
+    for (let i: number = 3; i <= n; i++) {
+        res = a + b + c
+        a = b;
+        b = c;
+        c = res;
+    }
+    return res;
+}
+function tribonacciTab(n: number): number {
     // known/base cases
     // 0: 0
     // 1: 1
