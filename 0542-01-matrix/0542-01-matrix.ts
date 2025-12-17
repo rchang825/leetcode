8aad0aba51754182008145cfe9bf27996a404b3a
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2025-12-17 13:26:12 -0800

    Time: 56 ms (79.54%), Space: 69.2 MB (70.45%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.ts
+++ 0542-01-matrix/0542-01-matrix.ts
@@ -1,4 +1,53 @@
 function updateMatrix(mat: number[][]): number[][] {
+    // declare constants
+    const m = mat.length;
+    const n = mat[0].length;
+    // initialize res array to be all the zeros in mat
+    // all 0's will have a distance of 0 to the nearest 0
+    const res: number[][] = Array.from({ length: m }, () => new Array(n));
+    // declare a queue
+    const q: number[][] = [];
+    for (let r: number = 0; r < m; r++) {
+        for (let c: number = 0; c < n; c++) {
+            // if 0
+            if (mat[r][c] === 0) {
+                // add to queue by position and distance 0
+                res[r][c] = 0;
+                q.push([r, c, 0]);
+            } else {
+                res[r][c] = -1;
+            }
+        }
+    }
+    // while queue is not empty
+    while (q.length !== 0) {
+        // dequeue a current element
+        let [r, c, dist] = q.shift();
+        // check left, right, up, down neighbor of curr if in bounds
+        // if neighbor doesn't have distance yet
+            // set distance of neighbor = curr distance + 1
+            // push neighbor onto queue
+        if (c > 0 && res[r][c - 1] === -1) {
+            res[r][c - 1] = dist + 1;
+            q.push([r, c - 1, dist + 1]); 
+        }
+        if (c < n - 1 && res[r][c + 1] === -1) {
+            res[r][c + 1] = dist + 1;
+            q.push([r, c + 1, dist + 1]); 
+        }
+        if (r > 0 && res[r - 1][c] === -1) {
+            res[r - 1][c] = dist + 1;
+            q.push([r - 1, c, dist + 1]); 
+        }
+        if (r < m - 1 && res[r + 1][c] === -1) {
+            res[r + 1][c] = dist + 1;
+            q.push([r + 1, c, dist + 1]); 
+        }
+    }
+    // return res array
+    return res;
+}
+function updateMatrix1(mat: number[][]): number[][] {
     // constant variables
     const m = mat.length;
     const n = mat[0].length
