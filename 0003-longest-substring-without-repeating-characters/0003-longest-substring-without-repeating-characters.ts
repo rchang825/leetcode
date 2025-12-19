ea2c295a6212e39203e285dbe0b13c51b3c95560
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2025-12-19 12:31:07 -0800

    Time: 7 ms (55.95%), Space: 60 MB (55.2%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.ts
@@ -0,0 +1,28 @@
+function lengthOfLongestSubstring(s: string): number {
+  // sliding window
+  // frequency map to validate (can be a set)
+  let left: number = 0;
+  let right: number = 0;
+  let res = 0;
+  let chars = new Set();
+  // while left < right
+  while (left <= right && right < s.length) {
+    // if invalid
+    if (chars.has(s[right])) {
+        // remove character at left from map
+        chars.delete(s[left]);
+        // close left
+        left++;
+    } else {
+    // if valid
+        // add character at right to map
+        chars.add(s[right]);
+        // update res
+        res = Math.max(res, chars.size);
+        // open to right
+        right++;
+    }
+  }
+  // return res
+  return res;
+};
