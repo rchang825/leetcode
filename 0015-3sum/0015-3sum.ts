340c6a6bad6ddf3979963e44c12c7fad2a035aaf
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2025-12-22 13:38:47 -0800

    Time: 51 ms (25.29%), Space: 72.7 MB (77.44%) - LeetHub

---
+++ 0015-3sum/0015-3sum.ts
@@ -0,0 +1,43 @@
+function threeSum(nums: number[]): number[][] {
+    // sort nums
+    nums.sort((a, b) => a - b);
+    // define res
+    console.log(nums);
+    const res: number[][] = [];
+    const n: number = nums.length;
+    // fix first
+    for (let i: number = 0; i < n; i++) {
+        let curr = [nums[i]];
+        let currSum = nums[i];
+        // skip duplicates
+        if (i > 0 && nums[i] === nums[i - 1]) {
+            continue;
+        }
+        // first num must be negative
+        if (nums[i] > 0) {
+            continue;
+        }
+        // sliding window for j and k
+        let j: number = i + 1;
+        let k: number = n - 1;
+        while (j < k) {
+            let currSum: number = nums[i] + nums[j] + nums[k];
+            if (currSum > 0) {
+                // want a smaller max (close right)
+                k--;
+            } else if (currSum < 0) {
+                // want a larger min (close left)
+                j++;
+            } else {
+                // combination found, add to res
+                res.push([nums[i], nums[j], nums[k]]);
+                // move window over (skip dupes)
+                do {
+                  j++;  
+                } while (nums[j] === nums[j - 1] && j < nums.length) 
+            }
+        }
+    }
+    // return res
+    return res;
+};
