298922b88039048ab576d6bb37217285e9e9e2dc
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2025-12-29 13:54:37 -0800

    Time: 0 ms (100%), Space: 53.4 MB (92.7%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.js
+++ 0075-sort-colors/0075-sort-colors.js
@@ -9,75 +9,73 @@ var sortColors = function(nums) {
         // colors will be in order red, white, blue (0, 1, 2)
     // E: single element in nums -> nothing happens
 
-    // define constant of n = nums.length
     const n = nums.length;
-    // define a left and right pointer
-    // left pointer = 0
     let left = 0;
-    // find rightmost non-2 element
-    // right pointer = n - 1
-
     let right = n - 1;
-    // iterate through nums while left and right don't overlap
+    // loop while left and right pointers do not overlap
     while (left < right) {
-        // if current number at left = 2, 
-        if (nums[left] === 2) {
-            // swap element with element at right pointer
+        // check if a rule is violated
+        // if nums[left] is 2 or nums[right] is 0
+        if (nums[left] === 2 || nums[right] === 0) {
+            // swap
             let temp = nums[left];
             nums[left] = nums[right];
             nums[right] = temp;
-            // decrement right pointer
-            // right--;
         }
-        // increment left pointer if swap did something right -> nums[left] !== 2
+        // if nums[left] is not a 2
         if (nums[left] !== 2) {
+            // increment left
             left++;
         } else {
-            // otherwise, decrement right pointer
+            // decrement right to initiate different swap next iteration
+            right--;
+        }
+        // if nums[right] is 2
+        if (nums[right] === 2) {
+            // decrement right
             right--;
         }
     }
-    // console.log('nums after moving 2 to right', nums);
-    // reset left pointer to 0
+    /*
+        [2, 0, 2, 1, 1, 0]
+        [0, 0, 2, 1, 1, 2]
+               ^     ^ 
+        [0, 0, 1, 1, 2, 2]
+                  ^  
+        swap because 2 should go on right and 0 should go on left
+        increment left pointer bc it's not a 2
+        decrement right pointer bc it is a 2
+        no swap bc no "rules" violated
+        increment left
+        swap because 2 should go on right
+        increment left pointer bc it's not a 2
+        decrement right pointer bc it is a 2
+        pointers overlap, stop
+    */
+
+
+
+    while (left < right) {
+        if (nums[left] === 2) {
+            let temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+        }
+        if (nums[left] !== 2) {
+            left++;
+        } else {
+            right--;
+        }
+    }
     left = 0;
-    // reset right pointer to 0
     right = 0;
-    // iterate through nums while right pointer is within bounds of nums
     while (right < n) {
-        // if current number at right = 0
         if (nums[right] === 0) {
-            // swap element with element at left pointer
             let temp = nums[right];
             nums[right] = nums[left];
             nums[left] = temp;
-            // increment left pointer
             left++;
         }
-        // increment right pointer
         right++;
     }
-    // done, 0's are on left and 2's are on right
-    // don't return anything, array should be sorted
-
-    /*
-    [2, 1, 2, 0, 1, 0]
-    length of 6, last index = 5
-    we know relative positions: all 2's will be to the "right"
-    all 0's will be to the "left"
-    leave all the 1's in the "middle"
-
-    one pass: for loop with index or while loop with left and right
-    first get all 2's over to right
-    track index to swap with (starting at last index)
-    found 2 at index 0, swap with index 5, decrement swapping index
-    found 2 at index 2, swap with index 4
-    [0, 1, 1, 0, 2, 2]
-
-    second pass:
-    get all 0's to left
-    track index to swap with (starting at 0)
-    found 0 at index 0, swap with index 0, increment swapping index
-    found 0 at index 3, swap with index 1
-    [0, 0, 1, 1, 2, 2]
-    */
 };
