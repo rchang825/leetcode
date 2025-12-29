e4c03928256166696f131b57ce264a9893889a4c
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2025-12-29 14:24:28 -0800

    Time: 0 ms (100%), Space: 55.8 MB (13.28%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.js
+++ 0075-sort-colors/0075-sort-colors.js
@@ -3,41 +3,23 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function(nums) {
-    // I: array of numbers (representing red, white, blue), 0, 1, 2
-    // O: sorted array (modify existing array)
-    // C: sorting in-place, aiming for one pass and constant space
-        // colors will be in order red, white, blue (0, 1, 2)
-    // E: single element in nums -> nothing happens
-
     const n = nums.length;
     let left = 0;
     let right = n - 1;
-    // define mid pointer
     let mid = 0;
-    // loop while left and right pointers do not overlap
     while (mid <= right) {
-        // check if a rule is violated
-        // if nums[mid] is 1
         if (nums[mid] === 1) {
-            // increment mid pointer
             mid++;
         } else if (nums[mid] === 0) {
-        // if nums[mid] is 0
-            // swap left with mid
             let temp = nums[left];
             nums[left] = nums[mid];
             nums[mid] = temp;
-            // increment left
             left++;
-            // increment mid
             mid++;
         } else {
-        // if nums[mid] is 2
-            // swap: mid with right
             let temp = nums[mid];
             nums[mid] = nums[right];
             nums[right] = temp;
-            // decrement right (we know nums[right] will be a 2)
             right--;
         }
     }
