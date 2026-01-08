b9e11bae2ee39caff23f1f054be18c298087223a
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2026-01-07 21:23:44 -0800

    Time: 11 ms (42.9%), Space: 66.9 MB (33.59%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.ts
+++ 0056-merge-intervals/0056-merge-intervals.ts
@@ -10,16 +10,15 @@ function merge(intervals: number[][]): number[][] {
     let i = 0;
     while (i < intervals.length) {
         let [currStart, currEnd]: number[] = intervals[i];
-        let j = i + 1;
-        while (j < intervals.length && intervals[j][0] <= currEnd) {
+        while (i < intervals.length - 1 && intervals[i + 1][0] <= currEnd) {
         // merge condition: while proceeding interval start is <= current end
             // keep first interval start, and max of ends
-            currEnd = Math.max(currEnd, intervals[j][1]);
-            j++;
+            currEnd = Math.max(currEnd, intervals[i + 1][1]);
+            i++;
         }
         // add in merged or unmerged interval
         res.push([currStart, currEnd]);
-        i = j;
+        i++;
     }
     // return res
     return res;
