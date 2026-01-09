8c0964e8917ed30343777d52ce818404a7a48318
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2026-01-09 13:45:26 -0800

    Time: 11 ms (80.47%), Space: 61.5 MB (92.13%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.ts
+++ 0207-course-schedule/0207-course-schedule.ts
@@ -1,48 +1,26 @@
 function canFinish(numCourses: number, prerequisites: number[][]): boolean {
-    // graph (adjacency list)
-    // keep a visited list
-    /*
-        [[0,1],[1,2],[2,3],[3,4]] (5 courses)
-        0: 1
-        1: 2
-        2: 3
-        3: 4
-        4: 
-        push on prereqs
-        detect cycles
-        pr of 0 is 1, visit 1, pr of 1, visit 2, pr of 2, visit 3, pr of 3, visit 4, no prs
-        empty -> can take all courses
-        [[0,1],[1,2],[2,0],[3,2]] (4 courses)
-        0: 1
-        1: 2
-        2: 0
-        3: 2
-        pr of 0 is 1, visit 1, pr of 1, visit 2, pr of 2, visit 0, 0 already visited!
-        cycle -> cannot take all courses
-    */
-    const indegrees: number[] = new Array(numCourses).fill(0);
+    const reliances: number[] = new Array(numCourses).fill(0);
     const adjList: number[][] = new Array(numCourses).fill(null).map(() => new Array());
     for (let i: number = 0; i < prerequisites.length; i++) {
         let [course, prereq] = prerequisites[i];
         adjList[prereq].push(course);
-        indegrees[course]++;
+        reliances[course]++;
     }
-    // console.log(adjList);
-    let visited = 0;
-    const queue = [];
+    let visited: number = 0;
+    const queue: number[] = [];
     for (let i: number = 0; i < numCourses; i++) {
-        if (indegrees[i] === 0) {
+        if (reliances[i] === 0) {
             queue.push(i);
         }
     }
     while (queue.length > 0) {
-        let curr = queue.shift();
+        let curr: number = queue.shift();
         visited++;
         adjList[curr].forEach((neighbor: number) => {
-            if (indegrees[neighbor] === 1) {
+            if (reliances[neighbor] === 1) {
                 queue.push(neighbor);
             } else {
-                indegrees[neighbor]--;
+                reliances[neighbor]--;
             }
         });
     }
