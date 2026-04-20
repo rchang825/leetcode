80e101398df1bcc1c144bb4f33c17911e8722ece
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2026-04-20 13:40:49 -0700

    Time: 10 ms (24.97%), Space: 60.9 MB (77.52%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.js
+++ 0012-integer-to-roman/0012-integer-to-roman.js
@@ -3,74 +3,18 @@
  * @return {string}
  */
 var intToRoman = function(num) {
-    // make map of dec to rom
-    const toRoman = {
-        '1': 'I',
-        '5': 'V',
-        '10': 'X',
-        '50': 'L',
-        '100': 'C',
-        '500': 'D',
-        '1000': 'M',
-        '4': 'IV',
-        '9': "IX",
-        '40': 'XL',
-        '90': 'XC',
-        '400': 'CD',
-        '900': 'CM',
-    };
-    // const toSubRoman = {
-    //     '4': 'IV',
-    //     '9': "IX",
-    //     '40': 'XL',
-    //     '90': 'XC',
-    //     '400': 'CD',
-    //     '900': 'CM',
-    // }
+    const r = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+    const d = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
 
-    const toRomanArr = Object.keys(toRoman).sort((a, b) => parseInt(b) - parseInt(a));
-    // const toSubRomanArr = Object.keys(toSubRoman).sort((a, b) => parseInt(b) - parseInt(a));
     let roman = [];
-    while (num > 0) {
-    // convert num to string
-    // let numStr = num.toString();
-    // console.log('numStr', numStr);
-    // let starts = numStr[0];
-    let freq = 1;
-    
-    // if number does not start with 4 or 9
-    // if (starts !== '4' && starts !== '9') {
-        // find max dec (iteratively)
-        for (let i = 0; i < toRomanArr.length; i++) {
-            let dec = parseInt(toRomanArr[i])
-            if (num % dec < num) {
-                // freq = number / max dec
-                freq = Math.floor(num / dec);
-                // roman += max roman * freq
-                for (let j = 0; j < freq; j++) {
-                    roman.push(toRoman[dec]);
-                }
-                // number -= max dec * freq
-                num -= dec * freq;
-                break;
-            }
+    for (let i = 0; i < r.length; i++) {
+        let dec = d[i];
+        while (dec <= num) {
+            roman.push(r[i]);
+            num -= dec;
         }
-    // }
-    // if number starts with 4 or 9
-    //  else {
-    //     // check for 4, 9, 40, 400, 900
-    //     for (let i = 0; i < toSubRomanArr.length; i++) {
-    //         let dec = parseInt(toSubRomanArr[i])
-    //         if (num % dec < num) {
-    //             // roman += max roman
-    //             roman.push(toSubRoman[dec]);
-    //             // number -= max dec
-    //             num -= dec;
-    //             break;
-    //         }
-    //     }
-    //  }
+        if (num === 0) {
+            return roman.join('');
+        }
     }
-    // return roman
-    return roman.join('');
 };
