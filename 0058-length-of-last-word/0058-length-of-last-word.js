8b957b6ae908f50a36815b73482c6d7fae0a00e1
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2026-04-22 12:22:38 -0700

    Time: 0 ms (100%), Space: 53.1 MB (86.45%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.js
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -3,6 +3,21 @@
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
+    // traverse from back to find end of last word
+    for (let i = s.length - 1; i >= 0; i--) {
+        if (s[i] !== ' ') {
+            // keep traversing until start of last word
+            let length = 1;
+            i--;
+            while (i >= 0 && s[i] !== ' ') {
+                i--;
+                length++;
+            }
+            return length;
+        }
+    }
+};
+var lengthOfLastWordFirst = function(s) {
     // break up string into array of words divided by spaces
     let almost = s.split(' ');
     // traverse from back to find last word
