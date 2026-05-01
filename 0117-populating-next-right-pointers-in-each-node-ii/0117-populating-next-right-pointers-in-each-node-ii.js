307d46065981e3bd887aa87db33dd55f02837dba
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2026-05-01 12:22:27 -0700

    Time: 64 ms (21.65%), Space: 60.5 MB (14.33%) - LeetHub

--- 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.js
+++ 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.js
@@ -16,29 +16,29 @@ var connect = function(root) {
     if (!root) {
         return root;
     }
-    // level order traversal: stack
+    // level order traversal: queue
     // keep track of level!
-    // define stack
-    let stk = [];
+    // define queue
+    let q = [];
     // root stays unchanged because no right nodes
-    // add first children to stack
+    // add first children to queue
     if (root.left) {
-        stk.push(root.left);
+        q.push(root.left);
     }
     if (root.right) {
-        stk.push(root.right);
+        q.push(root.right);
     }
-    // loop while stack not empty
-    while (stk.length) {
+    // loop while queue not empty
+    while (q.length) {
         // loop through all possible nodes in level including null nodes
-        let levelSize = stk.length;
+        let levelSize = q.length;
         // console.log('this level has', levelSize, 'nodes');
         for (let i = 0; i < levelSize; i++) {
-            // curr is stack pop
-            let curr = stk.shift();
+            // curr is queue dequeue
+            let curr = q.shift();
             if (curr) {
                 if (i !== levelSize - 1) {
-                    curr.next = stk[0];
+                    curr.next = q[0];
                     // console.log(curr.val, 'points to', curr.next.val);
                 } else {
                     // console.log(curr.val, 'is the rightmost node and points to nothing!');
@@ -46,10 +46,10 @@ var connect = function(root) {
                 
                 // add curr left and right children only if they exist
                 if (curr.left) {
-                    stk.push(curr.left);
+                    q.push(curr.left);
                 }
                 if (curr.right) {
-                    stk.push(curr.right);
+                    q.push(curr.right);
                 }
             }
         }
