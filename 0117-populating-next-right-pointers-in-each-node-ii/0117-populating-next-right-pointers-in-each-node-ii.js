12fabaf40343f664a12dad02f4c12060e354645a
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2026-05-01 12:23:08 -0700

    Time: 50 ms (90.24%), Space: 60.2 MB (26.83%) - LeetHub

--- 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.js
+++ 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.js
@@ -18,33 +18,21 @@ var connect = function(root) {
     }
     // level order traversal: queue
     // keep track of level!
-    // define queue
     let q = [];
-    // root stays unchanged because no right nodes
-    // add first children to queue
     if (root.left) {
         q.push(root.left);
     }
     if (root.right) {
         q.push(root.right);
     }
-    // loop while queue not empty
     while (q.length) {
-        // loop through all possible nodes in level including null nodes
         let levelSize = q.length;
-        // console.log('this level has', levelSize, 'nodes');
         for (let i = 0; i < levelSize; i++) {
-            // curr is queue dequeue
             let curr = q.shift();
             if (curr) {
                 if (i !== levelSize - 1) {
                     curr.next = q[0];
-                    // console.log(curr.val, 'points to', curr.next.val);
-                } else {
-                    // console.log(curr.val, 'is the rightmost node and points to nothing!');
-                }
-                
-                // add curr left and right children only if they exist
+                } 
                 if (curr.left) {
                     q.push(curr.left);
                 }
@@ -54,6 +42,5 @@ var connect = function(root) {
             }
         }
     }
-    // return root
     return root;
 };
