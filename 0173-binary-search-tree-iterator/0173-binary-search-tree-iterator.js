04c2ae9dbe5dba0d7d1661092f9f22e633a5d5da
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2026-05-04 14:07:30 -0700

    Time: 13 ms (13.23%), Space: 69.5 MB (28.92%) - LeetHub

---
+++ 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.js
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ */
+var BSTIterator = function(root) {
+    this.trav = [];
+    this.helper = function(curr) {
+        if (!curr) {
+            return;
+        } 
+        this.helper(curr.left);
+        this.trav.push(curr.val);
+        this.helper(curr.right);
+    }
+    this.helper(root);
+    nextIn = 0;
+};
+
+/**
+ * @return {number}
+ */
+BSTIterator.prototype.next = function() {
+    let val = this.trav[nextIn];
+    nextIn++;
+    return val;
+};
+
+/**
+ * @return {boolean}
+ */
+BSTIterator.prototype.hasNext = function() {
+    return nextIn < this.trav.length;
+};
+
+/** 
+ * Your BSTIterator object will be instantiated and called as such:
+ * var obj = new BSTIterator(root)
+ * var param_1 = obj.next()
+ * var param_2 = obj.hasNext()
+ */
