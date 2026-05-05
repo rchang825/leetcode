3d54b8aad0b6a96d4ef7cd0dc7eb88ae522ab0db
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2026-05-05 13:34:40 -0700

    Time: 48 ms (9.11%), Space: 78.8 MB (73.21%) - LeetHub

--- 0148-sort-list/0148-sort-list.js
+++ 0148-sort-list/0148-sort-list.js
@@ -10,6 +10,49 @@
  * @return {ListNode}
  */
 var sortList = function(head) {
+    // divide and conquer with merge sort
+    // need middle element
+    var findMiddle = function(curr) {
+        let fast = curr;
+        let slow = curr;
+        let prev = null;
+        while (fast && fast.next) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        prev.next = null;
+        return slow;
+    }
+    var merge = function(l1, l2) {
+        let newHead = new ListNode();
+        let curr = newHead;
+        while (l1 && l2) {
+            if (l1.val < l2.val) {
+                curr.next = l1;
+                l1 = l1.next;
+            } else {
+                curr.next = l2;
+                l2 = l2.next;
+            }
+            curr = curr.next;
+        }
+        if (l1) {
+            curr.next = l1;
+        } else {
+            curr.next = l2;
+        }
+        return newHead.next;
+    }
+    if (!head || !head.next) {
+        return head;
+    }
+    let mid = findMiddle(head);
+    let left = sortList(head);
+    let right = sortList(mid);
+    return merge(left, right);
+};
+var sortListA = function(head) {
     // iterate through list and save values
     let curr = head;
     let trav = [];
