6c55f9a10262396fc1faa7babf0e7681e695052c
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2026-05-06 13:44:58 -0700

    Time: 231 ms (5.08%), Space: 159.5 MB (5.07%) - LeetHub

--- 0077-combinations/0077-combinations.js
+++ 0077-combinations/0077-combinations.js
@@ -4,14 +4,21 @@
  * @return {number[][]}
  */
 var combine = function(n, k) {
+    // number of combinations = k * (n / k)
     // define res
     const res = [];
     // backtracking helper(curr, index)
     var backtrack = function(curr, i) {
+        // console.log(curr);
         // if length of curr = k
         if (curr.length === k) {
             // add to res
             res.push(curr);
+        }
+        // if length of curr < k and not enough, return early
+        if (k - curr.length > n - i) {
+            // console.log('returning early bc impossible to go down this path!');
+            return;
         }
         // for each from index + 1 to n
         for (let j = i + 1; j <= n; j++) {
@@ -20,7 +27,7 @@ var combine = function(n, k) {
         }
     };
     // loop through from 1 to n
-    for (let i = 1; i <= n; i++) {
+    for (let i = 1; i <= n - k + 1; i++) {
         // call backtrack([index], index)
         backtrack([i], i);
     }
