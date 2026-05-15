932dd7f210ceac14d30d16c5a7a6aef13b4f8eed
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2026-05-15 11:35:19 -0700

    Time: 0 ms (100%), Space: 54 MB (36.21%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.js
+++ 0162-find-peak-element/0162-find-peak-element.js
@@ -2,17 +2,21 @@
  * @param {number[]} nums
  * @return {number}
  */
-var findPeakElement = function(nums) {
-    // iterate through nums
-    for (let i = 0; i < nums.length; i++) {
-        // prev = nums[i - 1] or negative inf
-        let prev = i === 0 ? Number.NEGATIVE_INFINITY : nums[i - 1];
-        // next = nums[i + 1] or negative inf
-        let next = i === nums.length - 1 ? Number.NEGATIVE_INFINITY : nums[i + 1];
-        let curr = nums[i];
-        // at each point, if prev < curr > next, return index of curr
-        if (prev < curr && curr > next) {
-            return i;
-        }
+var findPeakElement = function(nums, l = 0, r = nums.length) {
+    // midpoint
+    let midIndex = Math.floor((l + r - 1) / 2);
+    let midpoint = nums[midIndex];
+    let prev = midIndex === 0 ? Number.NEGATIVE_INFINITY : nums[midIndex - 1];
+    let next = midIndex + 1 < nums.length ? nums[midIndex + 1] : Number.NEGATIVE_INFINITY;
+    // base case: midpoint is peak
+    if (prev < midpoint && midpoint > next) {
+        // return midIndex
+        return midIndex;
+    }
+    // otherwise, decide which half to accept (greater)
+    if (next > midpoint) {
+        return findPeakElement(nums, midIndex + 1, r);
+    } else {
+        return findPeakElement(nums, l, midIndex + 1);
     }
 };
