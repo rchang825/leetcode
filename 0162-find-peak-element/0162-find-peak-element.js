37aeaed4a4254bee764c0e70517bdef368c76565
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2026-05-15 11:39:39 -0700

    Time: 0 ms (100%), Space: 53.3 MB (82.94%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.js
+++ 0162-find-peak-element/0162-find-peak-element.js
@@ -2,21 +2,19 @@
  * @param {number[]} nums
  * @return {number}
  */
-var findPeakElement = function(nums, l = 0, r = nums.length) {
-    // midpoint
-    let midIndex = Math.floor((l + r - 1) / 2);
-    let midpoint = nums[midIndex];
-    let prev = midIndex === 0 ? Number.NEGATIVE_INFINITY : nums[midIndex - 1];
-    let next = midIndex + 1 < nums.length ? nums[midIndex + 1] : Number.NEGATIVE_INFINITY;
-    // base case: midpoint is peak
-    if (prev < midpoint && midpoint > next) {
-        // return midIndex
-        return midIndex;
+var findPeakElement = function(nums, l = 0, r = nums.length - 1) {
+    // base case: single element -> peak
+    if (l === r) {
+        return l;
     }
-    // otherwise, decide which half to accept (greater)
+    // midpoint
+    let midIndex = Math.floor((l + r) / 2);
+    let midpoint = nums[midIndex];
+    let next = nums[midIndex + 1];
+    // otherwise, decide which half to accept (greater or default right)
     if (next > midpoint) {
         return findPeakElement(nums, midIndex + 1, r);
     } else {
-        return findPeakElement(nums, l, midIndex + 1);
+        return findPeakElement(nums, l, midIndex);
     }
 };
