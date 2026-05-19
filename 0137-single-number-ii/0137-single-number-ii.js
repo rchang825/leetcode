3de0d0d1024caa2080e3f6c8d41bfdffebcc2236
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2026-05-19 11:03:35 -0700

    Time: 8 ms (31.87%), Space: 58.7 MB (12.91%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.js
+++ 0137-single-number-ii/0137-single-number-ii.js
@@ -3,11 +3,16 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-    nums.sort((a, b) => a - b);
-    for (let i = 0; i < nums.length - 1; i += 3) {
-        if (nums[i] !== nums[i + 1]) {
-            return nums[i];
+    let res = 0;
+
+    for (let i = 0; i < 32; i++) {
+        let bitSum = 0;
+        for (let n of nums) {
+            let bit = (n >> i) & 1;
+            bitSum += bit;
         }
+        let leftover = bitSum % 3;
+        res = res | (leftover << i);
     }
-    return nums[nums.length - 1];
+    return res;
 };
