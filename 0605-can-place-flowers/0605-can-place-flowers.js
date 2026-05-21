c70f75ba0d4b336530947f690f5a063c65ce6eb9
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2026-05-21 11:49:11 -0700

    Time: 0 ms (100%), Space: 56.2 MB (54.63%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.js
+++ 0605-can-place-flowers/0605-can-place-flowers.js
@@ -3,34 +3,31 @@
  * @param {number} n
  * @return {boolean}
  */
-var canPlaceFlowers = function(flowerbed, n) {
+var canPlaceFlowers = function (flowerbed, n) {
     if (n === 0) {
         return true;
     }
     let max = 0;
     // iterate through flowerbed 
     for (let i = 0; i < flowerbed.length; i++) {
+        if (flowerbed[i] === 1) {
+            continue;
+        }
         // "plant" whenever possible, increment max
         if (i === 0) {
             if (i === flowerbed.length - 1 || flowerbed[i + 1] === 0) {
-                if (flowerbed[i] === 0) {
-                    flowerbed[i] = 1;
-                    max++;
-                }
+                flowerbed[i] = 1;
+                max++;
             }
         } else if (i === flowerbed.length - 1) {
             if (flowerbed[i - 1] === 0) {
-                if (flowerbed[i] === 0) {
-                    flowerbed[i] = 1;
-                    max++;
-                }
+                flowerbed[i] = 1;
+                max++;
             }
         } else {
             if (flowerbed[i - 1] === 0 && flowerbed[i + 1] === 0) {
-                if (flowerbed[i] === 0) {
-                    flowerbed[i] = 1;
-                    max++;
-                }
+                flowerbed[i] = 1;
+                max++;
             }
         }
     }
