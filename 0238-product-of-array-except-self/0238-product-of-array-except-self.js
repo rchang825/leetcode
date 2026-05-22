6739e24bdc48eaac8d582d90414a9e181e491ea0
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2026-05-22 11:26:21 -0700

    Time: 6 ms (64.17%), Space: 71.4 MB (42.41%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -4,22 +4,17 @@
  */
 var productExceptSelf = function(nums) {
     // prefix product
-    let pref = [];
+    let res = [];
     let prod = 1;
     for (let i = 0; i < nums.length; i++) {
-        pref.push(prod);
+        res.push(prod);
         prod *= nums[i];
     }
     // suffix product
-    let suff = new Array(nums.length);
     prod = 1;
     for (let i = nums.length - 1; i >= 0; i--) {
-        suff[i] = prod;
+        res[i] *= prod;
         prod *= nums[i];
     }
-    // multiply pref and suff to get product except self
-    for (let i = 0; i < nums.length; i++) {
-        nums[i] = pref[i] * suff[i];
-    }
-    return nums;
+    return res;
 };
