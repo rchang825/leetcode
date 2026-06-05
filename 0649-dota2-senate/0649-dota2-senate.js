a59ed1c91f52ce3dc6a6ede91b22c524fa089cac
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2026-06-05 14:00:34 -0700

    Time: 23 ms (22.34%), Space: 57 MB (89.34%) - LeetHub

---
+++ 0649-dota2-senate/0649-dota2-senate.js
@@ -0,0 +1,62 @@
+/**
+ * @param {string} senate
+ * @return {string}
+ */
+var predictPartyVictory = function(senate) {
+    // add all senators to queue
+    // count how many Ds
+    // counter of how many Ds are banned
+    // ban opposing senator (pop off queue)
+    // announce victory (if bannedDNum = dNum)
+    /*
+    RRDRD -> RRR
+    dNum = 2
+    R: pop, ban next D, back on queue, dNum = 1
+    R: pop, ban next D, back on queue, dNum = 0 -> RADIANT
+    D: pop, skipped, bDNum = 1, does not go back
+    R: pop, ban next D, back on queue, bDNum = 2
+    D: pop, skipped, bDNum = 1, does not go back
+
+
+    DDRDR
+    dNum = 3
+    D: pop, ban next R, back on queue, dNum = 4
+    D: pop, ban next R, back on queue, dNum = 5 -> DIRE
+    */
+    let dNum = 0;
+    for (let i = 0; i < senate.length; i++) {
+        if (senate[i] === 'D') {
+            dNum++;
+        } 
+    }
+    let queue = senate.split('');
+    let bannedD = 0;
+    let bannedR = 0;
+    while (queue.length) {
+        // if bannedR or bannedD go beyond number of senators, decare victory
+        if (bannedR > senate.length) {
+            return 'Dire';
+        }
+        if (bannedD > senate.length) {
+            return 'Radiant';
+        }
+        let curr = queue.shift();
+        if (curr === 'R') {
+            if (bannedR > 0) {
+                bannedR--;
+            } else {
+                // ban D and go back on queue
+                bannedD++;
+                queue.push(curr);
+            }
+        } else {
+            if (bannedD > 0) {
+                bannedD--;
+            } else {
+                // ban R and go back on queue
+                bannedR++;
+                queue.push(curr);
+            }
+        }
+    }
+};
