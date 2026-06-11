8dae40ac302035b69cb7579dbd5199f82ea336f8
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2026-06-11 13:07:47 -0700

    Time: 0 ms (100%), Space: 54.5 MB (94.81%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.js
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.js
@@ -11,31 +11,29 @@
  * @return {number[]}
  */
 var rightSideView = function(root) {
-    // level order traversal but add right children first
-    // keep track of height and only add first of height to res
-    // root = height 0 -> add 1
-    // 3 (h=1), 2 (h=1) -> add 3
-    // 4 (h=2) -> add 4
-    // 5 (h=3) -> add 5
-
+    // level order trav
+    // one node from each level gets accepted (last/right-most node)
+    // BST with queue and track level sizes
+    if (!root) {
+        return [];
+    }
     let queue = [];
     let res = [];
-    queue.push([root, 0]);
-    while (queue.length !== 0) {
-        let curr = queue.shift();
-        let height = curr[1];
-        if (curr[0] !== null) {
-            res.push(curr[0].val);
-            queue.push([curr[0].right, height + 1]);
-            queue.push([curr[0].left, height + 1]);
-            while (queue.length !== 0 && height === queue[0][1]) {
-                let ignored = queue.shift();
-                if (ignored[0] !== null) {
-                    queue.push([ignored[0].right, ignored[1] + 1]);
-                    queue.push([ignored[0].left, ignored[1] + 1]);
-                }
-            } // ignore all other nodes on same level but add their children!
+    queue.push(root);
+    while (queue.length) {
+        let levelSize = queue.length;
+        let curr;
+        for (let i = 0; i < levelSize; i++) {
+           curr = queue.shift();
+           if (curr.left) {
+            queue.push(curr.left);
+           }
+           if (curr.right) {
+            queue.push(curr.right);
+           }
         }
+        // curr node will be last node in level
+        res.push(curr.val);
     }
     return res;
 };
