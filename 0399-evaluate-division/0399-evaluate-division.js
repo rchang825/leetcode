bd9ef30a0e42bf8515e86f33b335b8f2c0c5c57e
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2026-06-16 15:34:45 -0700

    Time: 0 ms (100%), Space: 53.7 MB (56.08%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.js
+++ 0399-evaluate-division/0399-evaluate-division.js
@@ -5,52 +5,71 @@
  * @return {number[]}
  */
 var calcEquation = function(equations, values, queries) {
-    // a / b = 2 -> a / b = 2 and b / a = 1/2
-    const edges = new Map();
+    // equation[i] -> [a, b], a -> b has weight of values[i], representing a / b = weight
+    /* 
+        equations = [["a","b"],["b","c"]], 
+        values = [2.0,3.0], 
+        queries = [["a","c"],["b","a"],["a","e"],["a","a"],["x","x"]]
 
+        a: [b, 2.0]
+        b: [a, 0.5], [c, 3.0]
+        c: [b, 0.3333]
+        
+        a -> c = a -> b (2.0) -> c (3.0) = 6.0
+        b -> a = b -> a (0.5)
+        c -> a = c -> b (0.333) -> a (0.5) = 0.167
+
+        make adj list using all known variables in equations
+        for each element e in equations equations[i]
+            create edge from e[0] to e[1] with weight values[i]
+            create edge from e[1] to e[0] with weight 1 / values[i]
+        find path from query[0] to query[1] and perform * on weights
+    */
+    let adj = new Map();
     for (let i = 0; i < equations.length; i++) {
-        let [dividend, divisor] = equations[i];
-        if (!edges.has(dividend)) {
-            edges.set(dividend, new Map());
-        }
-        if (!edges.has(divisor)) {
-            edges.set(divisor, new Map());
-        }
-        edges.get(dividend).set(divisor, values[i]);
-        edges.get(divisor).set(dividend, 1 / values[i]);
-    }
-
-    var backtrack = function(dividend, divisor, interm, visited) {
-        visited[dividend] = true;
-        let res = -1.0;
-        let neighbors = edges.get(dividend);
-        if (neighbors.has(divisor)) {
-            res = interm * neighbors.get(divisor);
+        let [a, b] = equations[i];
+        if (adj.has(a)) {
+            adj.get(a).push([b, values[i]]);
         } else {
-            for (const [nextNode, value] of neighbors.entries()) {
-                if (!visited[nextNode]) {
-                    res = backtrack(nextNode, divisor, interm * value, visited);
-                    if (res !== -1.0) {
-                        break;
-                    }
+            adj.set(a, [[b, values[i]]]);
+        }
+        if (adj.has(b)) {
+            adj.get(b).push([a, 1 / values[i]]);
+        } else {
+            adj.set(b, [[a, 1 / values[i]]]);
+        }
+    }
+    var calculate = function(a, b, quotient, visited) {
+        // dfs to find any path from a to b and return the product of weights in path or return -1.0
+        if (a === b) {
+            return quotient;
+        }
+
+        visited.add(a);
+
+        for (let node of adj.get(a)) {
+            if (!visited.has(node[0])) {
+                let res = calculate(node[0], b, quotient * node[1], visited);
+                if (res !== -1.0) {
+                    return res;
                 }
             }
+            
         }
-        delete visited[dividend];
-        return res;
+        return -1.0;
     }
 
     let res = [];
+
     for (let q of queries) {
-        let [dividend, divisor] = q;
-        if (!edges.has(dividend) || !edges.has(divisor)) {
+        if (!adj.has(q[0]) || !adj.has(q[1])) {
             res.push(-1.0);
-        } else if (dividend === divisor) {
+        } else if (q[0] === q[1]) {
             res.push(1.0);
         } else {
-            visited = {};
-            res.push(backtrack(dividend, divisor, 1, visited));
+            res.push(calculate(q[0], q[1], 1.0, new Set()));
         }
     }
+
     return res;
-};
+}
