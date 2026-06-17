13d5ed7c369c323098f3b4e97a693ae9e3c42d76
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2026-06-17 12:53:40 -0700

    Time: 29 ms (61.05%), Space: 67.7 MB (45.35%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.js
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.js
@@ -9,8 +9,6 @@ var nearestExit = function (maze, entrance) {
     // start from entrance
     let q = [[entrance, 0]];
 
-    let visited = new Array(maze.length).fill(0).map(() => new Array(maze[0].length).fill(false));
-
     // bfs
     while (q.length) {
         // dequeue
@@ -25,30 +23,30 @@ var nearestExit = function (maze, entrance) {
         // queue up all unvisited . around location with dist + 1
         // up
         if (r > 0) {
-            if (maze[r - 1][c] === '.' && !visited[r - 1][c]) {
+            if (maze[r - 1][c] === '.') {
                 q.push([[r - 1, c], dist + 1]);
-                visited[r - 1][c] = true;
+                maze[r - 1][c] = '+';
             }
         }
         // down
         if (r < maze.length - 1) {
-            if (maze[r + 1][c] === '.' && !visited[r + 1][c]) {
+            if (maze[r + 1][c] === '.') {
                 q.push([[r + 1, c], dist + 1]);
-                visited[r + 1][c] = true;
+                maze[r + 1][c] = '+';
             }
         }
         // left
         if (c > 0) {
-            if (maze[r][c - 1] === '.' && !visited[r][c - 1]) {
+            if (maze[r][c - 1] === '.') {
                 q.push([[r, c - 1], dist + 1]);
-                visited[r][c - 1] = true;
+                maze[r][c - 1] = '+';
             }
         }
         // right
         if (c < maze[0].length - 1) {
-            if (maze[r][c + 1] === '.' && !visited[r][c + 1]) {
+            if (maze[r][c + 1] === '.') {
                 q.push([[r, c + 1], dist + 1]);
-                visited[r][c + 1] = true;
+                maze[r][c + 1] = '+';
             }
         }
     }
