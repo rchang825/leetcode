e29071ebb1f498869a4dffab697b4067ccc7bfee
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2026-06-23 15:20:49 -0700

    Time: 0 ms (100%), Space: 52.6 MB (92.34%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.js
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.js
@@ -3,27 +3,31 @@
  * @return {string[]}
  */
 var letterCombinations = function(digits) {
-    let letters = {
-        2: ['a', 'b', 'c'],
-        3: ['d', 'e', 'f'],
-        4: ['g', 'h', 'i'],
-        5: ['j', 'k', 'l'],
-        6: ['m', 'n', 'o'],
-        7: ['p', 'q', 'r', 's'],
-        8: ['t', 'u', 'v'],
-        9: ['w', 'x', 'y', 'z'],
-    }
-    let makeCombination = function(curr, index) {
-        if (index === digits.length) {
+    // map number to letter
+    const letters = new Map([
+        ['2', ['a', 'b', 'c']],
+        ['3', ['d', 'e', 'f']],
+        ['4', ['g', 'h', 'i']],
+        ['5', ['j', 'k', 'l']],
+        ['6', ['m', 'n', 'o']],
+        ['7', ['p', 'q', 'r', 's']],
+        ['8', ['t', 'u', 'v']],
+        ['9', ['w', 'x', 'y', 'z']],
+    ]);
+    const res = [];
+    // starting with a curr of empty string until curr length matches digits length,
+        // add on every possible letter for each digit, building permutations
+    var translate = function(curr, i) {
+        if (i === digits.length) {
             res.push(curr);
             return;
+        }
+        // otherwise
+        // recurse with every possible letter for next digit
+        for (let letter of letters.get(digits[i])) {
+            translate(curr + letter, i + 1);
         } 
-        letters[digits[index]].forEach((letter) => {
-            makeCombination(curr + letter, index + 1);
-        });
     }
-
-    let res = [];
-    makeCombination('', 0);
+    translate('', 0);
     return res;
 };
