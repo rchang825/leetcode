26f9828bb2788fc287875c5370135a1736029bc8
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2026-06-24 11:39:30 -0700

    Time: 0 ms (100%), Space: 53.9 MB (27.09%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var tribonacci = function(n) {
+    // known: T(0) = 0, T(1) = 1, T(2) = 1
+    // all others = T(n - 1) + T(n - 2) + T(n - 3)
+    // T(3) = T(2) + T(1) + T(0) = 1 + 1 + 0 = 2
+    // tabulation: build from 0 to 37
+    // memoization: build from n down to 0 (faster if n < 37)
+    const T = new Array(38);
+    T[0] = 0;
+    T[1] = 1;
+    T[2] = 1;
+    for (let i = 3; i < 38; i++) {
+        T[i] = T[i - 1] + T[i - 2] + T[i - 3];
+    }
+    return T[n];
+};
