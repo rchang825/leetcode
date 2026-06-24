2d74de288ad865bfd6a75cdc4084036f018f5bf6
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2026-06-24 11:47:03 -0700

    Time: 0 ms (100%), Space: 53.7 MB (36.89%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.js
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.js
@@ -3,6 +3,23 @@
  * @return {number}
  */
 var tribonacci = function(n) {
+    const T = new Array(38);
+    T[0] = 0;
+    T[1] = 1;
+    T[2] = 1;
+    
+    var getT = function(n) {
+        if (T[n] !== undefined) {
+            return T[n];
+        }
+        let res = getT(n - 1) + getT(n - 2) + getT(n - 3);
+        T[n] = res;
+        return res;
+    }
+    return getT(n);
+};
+
+var tribonacciTab = function(n) {
     // known: T(0) = 0, T(1) = 1, T(2) = 1
     // all others = T(n - 1) + T(n - 2) + T(n - 3)
     // T(3) = T(2) + T(1) + T(0) = 1 + 1 + 0 = 2
