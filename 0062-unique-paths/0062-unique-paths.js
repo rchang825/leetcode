9192b5470f2043e3539f2cc9340a12b9aaa42721
Ruby Chang <78234142+rchang825@users.noreply.github.com>
2026-06-25 15:00:42 -0700

    Time: 1 ms (57.26%), Space: 53.2 MB (78.58%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.js
+++ 0062-unique-paths/0062-unique-paths.js
@@ -4,32 +4,65 @@
  * @return {number}
  */
 var uniquePaths = function(m, n) {
-    let matrix = new Array(m).fill(new Array(n).fill(1));
-    for (var r = 1; r < m; r++) {
-        // let curr = new Array(n).fill(1);
-        for (var c = 1; c < n; c++) {
-            matrix[r][c] = matrix[r][c - 1] + matrix[r - 1][c];
+    /*
+    2,1 = 1
+    2,2 = 2
+    2,3 = 3
+    2,4 = 4
+    if either m or n = 2, paths = non-2 arg
+
+    m and n are interchangeable
+    for m = 3
+    n = 1: 1
+    n = 2: 3 = 1 + 2
+    n = 3: 6 = 3 + 3
+    n = 4: 10 = 6 + 4
+    n = 5: 15 = 10 + 5
+    n = 6: 21 = 15 + 6
+    n = 7: 28 = 21 + 7
+    A(n) = A(n - 1) + n ?
+    
+    for m = 4
+    n = 1: 1
+    2: 4
+    3: 10 = 4 + 6
+    4: 20 = 10 + 10
+    5: 35 = 20 + 15
+    6: 56 = 35 + 21
+    7: 84 = 56 + 28
+    A(n) = A(m, n - 1) + A(m - 1, n)
+    */
+    const paths = new Array(m).fill(0).map(() => new Array(n).fill(1));
+    
+    for (let r = 1; r < m; r++) {
+        for (let c = 1; c < n; c++) {
+            paths[r][c] = paths[r - 1][c] + paths[r][c - 1];
         }
     }
-    return matrix[m - 1][n - 1];
-}
-var uniquePathsBad = function(m, n, r = 0, c = 0) {
-    // start from top left
-    // base case: out of bounds, return 0;
-    if (r > m) {
-        return 0;
+    
+    return paths[m - 1][n - 1];
+};
+
+var uniquePathsA = function(m, n) {
+    // helper
+    var findPaths = function(r, c) {
+        // if out of bounds
+        if (r < 0 || r >= m || c < 0 || c >= n) {
+            // return
+            return 0;
+        }
+        // if at bottom right
+        if (r === m - 1 && c === n - 1) {
+            // increment paths by 1
+            return 1;
+        }
+        // recurse with path + down
+        let downPath = findPaths(r + 1, c);
+        // recurse with path + right
+        let rightPath = findPaths(r, c + 1);
+        return downPath + rightPath;
     }
-    if (c > n) {
-        return 0;
-    }    
-    // base case: at bottom right
-    if (r === m - 1 && c === n - 1) {
-        // return 1;
-        return 1;
-    }
-    // recursive case:
-    // recurse on down
-    // recurse on right
-    // return summation 
-    return uniquePaths(m, n, r + 1, c) + uniquePaths(m, n, r, c + 1);
+    // start with path of top left
+    // return paths
+    return findPaths(0, 0);
 };
